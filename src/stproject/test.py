190ac8eb424bf3a39c03927e66f1b4ec18d23711
Steven Pramono <pramonosteven@gmail.com>
2020-09-02 21:48:05 +0800

    XGB fe1 train, valid, test

--- src/stproject/test.py
+++ src/stproject/test.py
@@ -7,7 +7,11 @@ from rdkit import RDConfig
 from utils import *
 import re
 
-smiles = 'COC=O'
-mol = Chem.MolFromSmiles(smiles)
-csmiles = Chem.MolToSmiles(mol)
-print(Chem.Fragments.fr_C_O(mol))
+smiles = ['C(=O)O', 'CC(=O)O', 'COC=O', 'CC(=O)OC']
+names = ['formic acid', 'acetic acid', 'methyl formate', 'methyl acetate']
+mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+csmiles = [Chem.MolToSmiles(mol) for mol in mols]
+fr_COO = [Chem.Fragments.fr_COO(mol) for mol in mols]
+fr_ether = [Chem.Fragments.fr_ether(mol) for mol in mols]
+fr_ester = [Chem.Fragments.fr_ester(mol) for mol in mols]
+print(pd.DataFrame({'name': names, 'csmile': csmiles, 'fr_COO': fr_COO, 'fr_ether': fr_ether, 'fr_ester': fr_ester}))
